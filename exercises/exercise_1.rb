3f0b92aff4682ac25f09533661ad4396546bc153
adam-brendan <adamcraveiro@gmail.com>
2019-06-11 19:06:59 +0000

    Complete exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,19 +5,9 @@ puts "----------"
 
 # Your code goes below here ...
 
-ActiveRecord::Schema.define do
-    drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
-  
-    create_table :stores do |t|
-        t.string  :name
-        t.integer  :annual_revenue
-        t.string  :apparel
-    end
-end
-
-Store.create(name: "Burnaby", annual_revenue: 300000, apparel: "men's and women's")
-Store.create(name: "Richmond", annual_revenue: 1260000, apparel: "women's")
-Store.create(name: "Gastown", annual_revenue: 190000, apparel: "men's")
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: false, womens_apparel: false)
 
 stores = Store.count
 puts stores
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,16 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-Store.create(name: "Surrey", annual_revenue: 224000, apparel: "women's")
-Store.create(name: "Whistler", annual_revenue: 1900000, apparel: "men's")
-Store.create(name: "Yaletown", annual_revenue: 430000, apparel: "men's and women's")
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Store.where(apparel: "men's")
+@mens_stores = Store.where(mens_apparel: true).where(womens_apparel: false)
 
 puts "Men's Apparel Stores"
 @mens_stores.each { |store| puts "Name: #{store.name} / Annual Revenue: #{store.annual_revenue}" }
 
-@womens_stores = Store.where("annual_revenue < 1000000").where(apparel: "men's")
+@womens_stores = Store.where("annual_revenue < 1000000").where(mens_apparel: false).where(womens_apparel: false)
 
 puts "Women's Apparel Stores"
 @womens_stores.each { |store| puts "Name: #{store.name} / Annual Revenue: #{store.annual_revenue}" }
