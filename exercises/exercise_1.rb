b559165dae65f8717e51524a9b67a41c1de40db2
adam-brendan <adamcraveiro@gmail.com>
2019-06-11 17:14:05 +0000

    Complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,20 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+ActiveRecord::Schema.define do
+    drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
+  
+    create_table :stores do |t|
+        t.string  :name
+        t.integer  :annual_revenue
+        t.string  :apparel
+    end
+end
+
+Store.create(name: "Burnaby", annual_revenue: 300000, apparel: "men's and women's")
+Store.create(name: "Richmond", annual_revenue: 1260000, apparel: "women's")
+Store.create(name: "Gastown", annual_revenue: 190000, apparel: "men's")
+
+stores = Store.count
+puts stores
